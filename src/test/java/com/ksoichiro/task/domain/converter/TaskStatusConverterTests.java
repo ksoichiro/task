6e60ec3f6cb307d0ef35f1e93ca23c3a8b11eda7
Soichiro Kashima <soichiro.kashima@gmail.com>
2016-10-30 20:18:57 +0900

    Add tests for TaskStatusConverter

--- src/test/java/com/ksoichiro/task/domain/converter/TaskStatusConverterTests.java
+++ src/test/java/com/ksoichiro/task/domain/converter/TaskStatusConverterTests.java
@@ -2,37 +2,46 @@ package com.ksoichiro.task.domain.converter;
 
 import com.ksoichiro.task.App;
 import com.ksoichiro.task.constant.TaskStatusEnum;
+import com.ksoichiro.task.domain.Account;
+import com.ksoichiro.task.domain.Task;
+import com.ksoichiro.task.repository.AccountRepository;
 import com.ksoichiro.task.repository.TaskRepository;
-import org.junit.ClassRule;
-import org.junit.Rule;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.junit4.rules.SpringClassRule;
-import org.springframework.test.context.junit4.rules.SpringMethodRule;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 @Sql({"/truncate.sql", "/domain/data-task-status-converter.sql"})
 @SpringApplicationConfiguration(App.class)
-public class TaskStatusConverterTests {
-    @ClassRule
-    public static final SpringClassRule SPRING_CLASS_RULE = new SpringClassRule();
-
-    @Rule
-    public final SpringMethodRule springMethodRule = new SpringMethodRule();
+public class TaskStatusConverterTests extends AbstractTransactionalJUnit4SpringContextTests {
+    @Autowired
+    private AccountRepository accountRepository;
 
     @Autowired
     private TaskRepository taskRepository;
 
     @Test
-    public void convert() {
+    public void convertFromDatabase() {
         assertThat(taskRepository.findOne(1).getStatus(), is(TaskStatusEnum.NOT_YET));
         assertThat(taskRepository.findOne(2).getStatus(), is(TaskStatusEnum.DOING));
         assertThat(taskRepository.findOne(3).getStatus(), is(TaskStatusEnum.CANCELLED));
         assertThat(taskRepository.findOne(4).getStatus(), is(TaskStatusEnum.HOLD));
         assertThat(taskRepository.findOne(5).getStatus(), is(TaskStatusEnum.DONE));
+    }
+
+    @Test
+    public void convertToDatabase() {
+        Account account = accountRepository.findOne(1);
+
+        for (TaskStatusEnum e : TaskStatusEnum.values()) {
+            Task task = new Task("a", e, null, account);
+            task = taskRepository.save(task);
+            Integer status = Integer.parseInt(jdbcTemplate.queryForList("SELECT * FROM task WHERE id = ?", new Object[]{task.getId()}).get(0).get("status").toString());
+            assertThat(status, is(e.getCode()));
+        }
     }
 }
--- src/test/resources/domain/data-task-status-converter.sql
+++ src/test/resources/domain/data-task-status-converter.sql
@@ -1,3 +1,7 @@
+insert into `role` values (1, 'admin', 'Administrator', '2016-01-01 00:00:00', '2016-01-01 00:00:00');
+
+insert into `account` values (1, 'a', 'A', 'PASSWORD', 1, 1, '2016-10-01 09:00:00', '2016-10-01 09:00:00');
+
 insert into `task` values
 (1, 'Send a mail1', 0, 1, current_date(), '2016-10-02 09:00:00', '2016-10-02 09:00:00'),
 (2, 'Send a mail2', 1, 1, current_date(), '2016-10-02 09:00:00', '2016-10-02 09:00:00'),
