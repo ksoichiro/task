623ea1078d1146175c9016628fb0e84ae4d68772
Soichiro Kashima <soichiro.kashima@gmail.com>
2016-12-21 22:46:45 +0900

    Add test for DateUtils

--- src/test/java/com/ksoichiro/task/util/DateUtilsTests.java
+++ src/test/java/com/ksoichiro/task/util/DateUtilsTests.java
@@ -9,6 +9,14 @@ import static org.junit.Assert.assertThat;
 
 public class DateUtilsTests {
     @Test
+    public void truncateTime() {
+        String expr = "2016-12-11 10:01:30";
+        String pattern = "uuuu-MM-dd HH:mm:ss";
+        Date d = DateUtils.toDateFromString(expr, pattern);
+        assertThat(DateUtils.toStringFromDate(DateUtils.truncateTime(d), "uuuu-MM-dd HH:mm:ss"), is("2016-12-11 00:00:00"));
+    }
+
+    @Test
     public void parse() {
         String expr = "2016-12-11 10:01:30";
         String pattern = "uuuu-MM-dd HH:mm:ss";
